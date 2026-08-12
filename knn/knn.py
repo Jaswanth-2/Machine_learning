476a7c2069235350e35028228a46cde432b8ef5a
Jaswanth -2 <jaswanthramesh24@gmail.com>
2026-08-12 22:58:19 +0530

    updated eda

--- knn/knn.py
+++ knn/knn.py
@@ -1,6 +1,8 @@
 import numpy as np
-import pandas as pd
 
-data=pd.read_csv("D:\\ML\\Machine_learning\\knn\\Data\\train_data.csv")
-test=pd.read_csv("D:\\ML\\Machine_learning\\knn\\Data\\test_data.csv")
+class knn:
+    def __init__(self,k=5):
+        self.k=k
 
+    def fit(self,x,y):
+        
