da438d242fcbe957d43b69cb85ab3fafdb31bfed
Jaswanth -2 <jaswanthramesh24@gmail.com>
2026-08-12 21:59:54 +0530

    Update KNN EDA notebook

---
+++ knn/knn.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+data=pd.read_csv("D:\\ML\\Machine_learning\\knn\\Data\\train_data.csv")
+test=pd.read_csv("D:\\ML\\Machine_learning\\knn\\Data\\test_data.csv")
+
